280937c91574336cba998b2a749c2fe25d2402b2
Qihan Jiang <jiang.qihan@outlook.com>
2026-08-06 10:50:27 -0400

    add client works(temporary for WPP interview)

--- js/nav.js
+++ js/nav.js
@@ -7,6 +7,7 @@ const nav = `
     <li><a href="/about.html">About</a></li>
     <li><a href="/lab.html">Lab</a></li>
     <li><a href="/teaching.html">Teaching</a></li>
+    <li><a href="https://qihan-jiang.notion.site/Client-Works-AI-Workflow-3b4db043546180929230c526e3e39c66?pvs=73">Client Works</a></li>
   </ul>
 </nav>
 `;
